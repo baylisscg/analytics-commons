210aed2d897884ca03d50c7b433f1c3042cb196b
irfan <irfan@irfan-xps>
2014-02-18 15:29:01 +1000

    * throw exception if there is an error replacing the windows characters in R scripts generated in windows * add logger class * indentation * rewrite script loader to use statisticsexception and improve code usage metrics
    Change-Id: I70f6fd5f70e76f7f3051f8bc35dcee4056b1916e

--- src/main/java/au/edu/uq/aurin/util/Rscript.java
+++ src/main/java/au/edu/uq/aurin/util/Rscript.java
@@ -2,6 +2,7 @@ package au.edu.uq.aurin.util;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.regex.PatternSyntaxException;
 
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
@@ -9,38 +10,38 @@ import org.slf4j.LoggerFactory;
 
 public class Rscript {
 
-	protected final static Logger LOG = LoggerFactory.getLogger(Rscript.class);
+  private static final Logger LOG = LoggerFactory.getLogger(Rscript.class);
 
-	// constructor
-	public Rscript() {
+  private Rscript() {
+  }
 
-	}
+  /**
+   * Loads the given R script and returns a String representation
+   * 
+   * @param scriptName
+   *          path and the name of R script. For example:
+   *          "/scripts/scriptName.r"
+   * @return {@link String} representation of the loaded R script
+   * @throws StatisticsException
+   *           If unable to load or parse the given scriptName
+   */
+  public static String load(String scriptName) throws StatisticsException {
 
-	/**
-	 * Loads the given R script and returns a String representation
-	 * 
-	 * @param scriptName
-	 *            path and the name of R script. 
-	 *            For example: "/scripts/scriptName.r"
-	 * @return {@link String} representation of the loaded R script
-	 * @throws IOException
-	 *             If unable to load the given scriptName
-	 */
-	public static String load(String scriptName) throws IOException {
+    String script = null;
 
-		String script = null;
+    try {
+      InputStream is = Rscript.class.getResourceAsStream(scriptName);
+      script = IOUtils.toString(is).replaceAll(
+          System.getProperty("line.separator"), IOUtils.LINE_SEPARATOR_UNIX);
+      is.close();
+      LOG.info("R Script Size: " + script.length() + " Bytes for " + scriptName);
+    } catch (IOException ioe) {
+      throw new StatisticsException("Unable to load R Script: " + scriptName, ioe);
+    } catch (PatternSyntaxException pse) {
+      throw new StatisticsException("Unable to Parse loaded R Script", pse);
+    }
 
-		InputStream is = Rscript.class.getResourceAsStream(scriptName);
-		if (is == null || scriptName.length() == 0) {
-			throw new IOException("Invalid Input Rscript: " + scriptName);
-		} else {
-			script = IOUtils.toString(is).replaceAll(
-					System.getProperty("line.separator"),
-					IOUtils.LINE_SEPARATOR_UNIX);
-			LOG.info("R Script Size: " + script.length() + " Bytes for " + scriptName);
-			LOG.debug("R Script: " + scriptName + "\n" + script + "\n");
-			is.close();
-		}
-		return script;
-	}
+    return script;
+  }
+
 }
