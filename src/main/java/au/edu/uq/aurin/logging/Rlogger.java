17b78b6b5b5c45919bdbceb95c69da2d0a6dfc69
irfan <irfan@irfan-xps>
2014-02-18 15:23:23 +1000

    * make constructor private * throw statistics exeption * keep conditionals within if() to a minimum * create getter and setters for logOptions for introspection
    Change-Id: I175d2b83c7684f254207004e5bfb3901a55fdf6a

--- src/main/java/au/edu/uq/aurin/logging/Rlogger.java
+++ src/main/java/au/edu/uq/aurin/logging/Rlogger.java
@@ -10,86 +10,98 @@ import org.rosuda.REngine.RList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import au.edu.uq.aurin.util.StatisticsException;
+
 public class Rlogger {
 
-	private static String LEVEL;
-	private static String DIRECTORY;
+  private static String LEVEL;
+  private static String DIRECTORY;
 
-	protected static final Logger LOG = LoggerFactory.getLogger(Rlogger.class);
+  private static final Logger LOG = LoggerFactory.getLogger(Rlogger.class);
 
-	public static REXP logOptions;
+  private static REXP logOptions;
 
-	public static void logger() throws REXPMismatchException, IOException {
-		logger("OFF", System.getProperty("java.io.tmpdir")); // logLevel = "OFF",
-															// logDirectory =
-															// java temp
-															// directory
-		LOG.info("Java Temp Path = {}", System.getProperty("java.io.tmpdir"));
-	}
+  private Rlogger() {
+  }
+  
+  public static void logger() throws StatisticsException {
+    // logLevel = "OFF", logDirectory = java temp directory
+    String tmpDir = System.getProperty("java.io.tmpdir");
+    logger("OFF", tmpDir);
+    LOG.info("Java Temp Path = {}", tmpDir);
+  }
 
-	public static void logger(String log_level, String log_directory)
-			throws REXPMismatchException, IOException {
-		setLog_level(log_level);
-		setLog_directory(log_directory);
-		setupREXPLogOptions();
-	}
+  public static void logger(String logLevel, String logDirectory)
+      throws StatisticsException {
+    setLogLevel(logLevel);
+    try {
+      setLogDirectory(logDirectory);
+    } catch (IOException e) {
+      throw new StatisticsException(e);
+    }
+    setupREXPLogOptions();
+  }
 
-	private static void setupREXPLogOptions() throws REXPMismatchException {
-		RList op = new RList();
-		op.put("LOG_LEVEL", new REXPString(Rlogger.LEVEL));
-		op.put("LOG_DIRECTORY", new REXPString(Rlogger.DIRECTORY));
-		logOptions = REXP.createDataFrame(op);
-	}
+  private static void setupREXPLogOptions() throws StatisticsException {
+    RList op = new RList();
+    op.put("LOG_LEVEL", new REXPString(Rlogger.LEVEL));
+    op.put("LOG_DIRECTORY", new REXPString(Rlogger.DIRECTORY));
+    try {
+      setLogOptions(REXP.createDataFrame(op));
+    } catch (REXPMismatchException e) {
+      throw new StatisticsException(e);
+    }
+  }
 
-	public static String getLog_level() {
-		LOG.info("current log level: {}", LEVEL);
-		return LEVEL;
-	}
+  public static String getLogLevel() {
+    LOG.info("current log level: {}", LEVEL);
+    return LEVEL;
+  }
 
-	private static void setLog_level(String log_level) {
-		if (log_level != null) {
-			if (log_level.isEmpty()) {
-				LEVEL = "OFF";
-			} else if (log_level.compareToIgnoreCase("OFF") == 0
-					|| log_level.compareToIgnoreCase("ERROR") == 0
-					|| log_level.compareToIgnoreCase("WARN") == 0
-					|| log_level.compareToIgnoreCase("INFO") == 0
-					|| log_level.compareToIgnoreCase("DEBUG") == 0
-					|| log_level.compareToIgnoreCase("TRACE") == 0) {
-				LEVEL = log_level.toUpperCase();
-			} else {
-				LEVEL = "OFF";
-			}
-		}
-		//
-		// if (log_level > 0 && log_level < 6) {
-		// LEVEL = log_level;
-		// } else if (log_level > 6) {
-		// LEVEL = 5; // max log level
-		// } else {
-		// LEVEL = 0; // no logging
-		// }
-	}
+  private static void setLogLevel(String logLevel) {
+    if (logLevel != null) {
+      if (logLevel.isEmpty() || (logLevel.compareToIgnoreCase("OFF") == 0)) {
+        LEVEL = "OFF";
+      } else if ((logLevel.compareToIgnoreCase("ERROR") == 0) || (logLevel.compareToIgnoreCase("WARN") == 0)) {
+        LEVEL = logLevel.toUpperCase();
+      } else if((logLevel.compareToIgnoreCase("INFO") == 0) || (logLevel.compareToIgnoreCase("DEBUG") == 0)) {
+        LEVEL = logLevel.toUpperCase();
+      } else if(logLevel.compareToIgnoreCase("TRACE") == 0) {
+        LEVEL = logLevel.toUpperCase();
+      } else {
+        LEVEL = "OFF";
+      }
+    }
+  }
 
-	public static String getLog_directory() {
-		LOG.info("current log directory: {}", DIRECTORY);
-		return DIRECTORY;
-	}
+  public static String getLogDirectory() {
+    LOG.info("current log directory: {}", DIRECTORY);
+    return DIRECTORY;
+  }
 
-	private static void setLog_directory(String log_directory)
-			throws IOException {
-		DIRECTORY = "."; // default log directory = "."
-		if (log_directory != null) {
-			if ((!log_directory.isEmpty())
-					&& (new File(log_directory).isDirectory())) {
-				DIRECTORY = log_directory;
-			} else {
-				String msg = "Log directory: " + log_directory + ", not found.";
-				LOG.info(msg);
-				DIRECTORY = null; // null logging
-				// throw new IOException(msg);
-			}
-		}
-	}
+  private static void setLogDirectory(String logDirectory)
+      throws IOException {
+    // default log directory = "."
+    DIRECTORY = ".";
+    if (logDirectory != null) {
+      if ((!logDirectory.isEmpty())
+          && (new File(logDirectory).isDirectory())) {
+        DIRECTORY = logDirectory;
+      } else {
+        String msg = "Log directory: " + logDirectory + ", not found.";
+        LOG.info(msg);
+        // null logging
+        DIRECTORY = null;
+      }
+    }
+  }
+
+  public static REXP getLogOptions() {
+    return logOptions;
+  }
+
+  public static void setLogOptions(REXP logOptions) {
+    Rlogger.logOptions = logOptions;
+  }
 
 }
