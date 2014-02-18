c8a970da829798ca02b1287b6545315c5d180a19
irfan <irfan@irfan-xps>
2014-02-18 15:26:50 +1000

    * use the statistics exception instead od throwing 3 different exceptions * indentation * remove modifiers in interface as they are by default public static final
    Change-Id: I4977cc5332577cbf6109d88524d57b48a6e0d648

--- src/main/java/au/edu/uq/aurin/interfaces/Statistics.java
+++ src/main/java/au/edu/uq/aurin/interfaces/Statistics.java
@@ -1,38 +1,33 @@
 package au.edu.uq.aurin.interfaces;
 
-import java.io.IOException;
-
-import org.rosuda.REngine.REXPMismatchException;
-import org.rosuda.REngine.Rserve.RserveException;
-
 import au.edu.uq.aurin.util.Rserve;
+import au.edu.uq.aurin.util.StatisticsException;
 
 /**
- * All R statistics packages must implement the
- * {@link Statistics} interface
+ * All R statistics packages must implement the {@link Statistics} interface
  * 
  * @author irfan
  * 
  */
 public interface Statistics {
 
-	/**
-	 * Computes the desired statistical algorithm implemented as an R-script
-	 * with an Rserve backend, connected via {@link Rserve}.
-	 * 
-	 * @throws RserveException
-	 *             Unable to connect to the Rserve instance
-	 * @throws IOException 
-	 *             Unable to load the R-script file for execution
-	 * @throws REXPMismatchException
-	 *             Result does not match the expected value
-	 */
-	public abstract void compute() throws RserveException, IOException,
-			REXPMismatchException;
-	
-	/**
-	 * Print the results in a readable, printable format
-	 */
-	public abstract String prettyPrint();
-	
+  /**
+   * Computes the desired statistical algorithm implemented as an R-script with
+   * an Rserve backend, connected via {@link Rserve}.
+   * 
+   * @throws StatisticsException
+   *           Unable to compute Statistical Computation
+   */
+  void compute() throws StatisticsException;
+
+  /**
+   * Print the results in a readable, printable format
+   *
+   *@return String representation of a Statistical Computation
+   *
+   * @throws StatisticsException
+   *           Unable to parse Statistical Computation
+   */
+  String prettyPrint() throws StatisticsException;
+
 }
