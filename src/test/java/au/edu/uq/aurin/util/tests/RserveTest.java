6c7169d81eed1bd998154a88a2c9fa943b03333d
irfan <irfan@irfan-xps>
2014-02-18 15:20:45 +1000

    * fix indentation * use apache commons for full stack trace of the exception
    Change-Id: Ifa37ad8d3442dfd34a06e2fb2f6de97600c22a99

--- src/test/java/au/edu/uq/aurin/util/tests/RserveTest.java
+++ src/test/java/au/edu/uq/aurin/util/tests/RserveTest.java
@@ -1,8 +1,7 @@
 package au.edu.uq.aurin.util.tests;
 
-
-import junit.framework.Assert;
-
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.junit.Assert;
 import org.junit.Test;
 import org.rosuda.REngine.Rserve.RConnection;
 import org.rosuda.REngine.Rserve.RserveException;
@@ -11,39 +10,36 @@ import au.edu.uq.aurin.util.Rserve;
 
 public class RserveTest {
 
-	/**
-	 * Test method to start and stop Rserve
-	 */
-	@Test
-	public void rserveTest() {
+  /**
+   * Test method to start and stop Rserve
+   */
+  @Test
+  public void rserveTest() {
 
-		Assert.assertTrue(
-				"Cannot Shutdown Rserve, Check if there are permissions "
-						+ "to shut it down if the process is owned by a different user",
-				Rserve.checkLocalRserve());
+    Assert.assertTrue("Cannot Shutdown Rserve, Check if there are permissions "
+        + "to shut it down if the process is owned by a different user",
+        Rserve.checkLocalRserve());
 
-		RConnection c = null;
+    Assert.assertTrue("Rserve Running?", Rserve.isRserveRunning());
+    RConnection c = null;
 
-		try {
-			// Make a connection if we successfully got Rserve running
-			Assert.assertNotNull("Cannot Connect to Rserve",
-					c = new RConnection());
+    try {
+      // Make a connection if we successfully got Rserve running
+      Assert.assertNotNull("Cannot Connect to Rserve", c = new RConnection());
 
-			System.out.println("got a connection, so closing the connection");
+      System.out.println("got a connection, so closing the connection");
 
-			Assert.assertTrue("Cannot Close connection to Rserve", c.close());
+      Assert.assertTrue("Cannot Close connection to Rserve", c.close());
 
-			Assert.assertTrue(
-					"Cannot Shutdown Rserve, Check if Rserve.conf is has "
-							+ "control enabled, Check if there are permissions to shut "
-							+ "it down if the process is owned by a different user",
-					Rserve.shutdownRserve());
-		} catch (RserveException re) {
-			re.printStackTrace();
-			Assert.fail("Rserve Exception");
-		} finally {
-			if (c != null)
-				c.close();
-		}
-	}
+      Assert.assertTrue("Cannot Shutdown Rserve, Check if Rserve.conf is has "
+          + "control enabled, Check if there are permissions to shut "
+          + "it down if the process is owned by a different user",
+          Rserve.shutdownRserve());
+    } catch (RserveException re) {
+      Assert.fail("Test Failed: " + ExceptionUtils.getFullStackTrace(re));
+    } finally {
+      if (c != null)
+        c.close();
+    }
+  }
 }
