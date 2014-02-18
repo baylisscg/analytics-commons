df363dc7e6779b468313e7ea96f3521e534d2532
irfan <irfan@irfan-xps>
2014-02-18 15:24:38 +1000

    * indentation * use full stack trace * use the new public getter to check the log options setup in R
    Change-Id: I798b2080fb3f52ff8d52bed927a266d3c3065fde

--- src/test/java/au/edu/uq/aurin/logging/tests/RloggerTest.java
+++ src/test/java/au/edu/uq/aurin/logging/tests/RloggerTest.java
@@ -1,47 +1,42 @@
 package au.edu.uq.aurin.logging.tests;
 
-import junit.framework.Assert;
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.junit.Assert;
 import org.junit.Test;
 
 import au.edu.uq.aurin.logging.Rlogger;
 
 public class RloggerTest {
 
-	@Test
-	public void test() {
-		System.out.println("--- RLogging Test ---");
+  @Test
+  public void test() {
 
-		try {
-			
-			// Sample usage from Java variables setup
-			String LOG_LEVEL = "BLAH";
-			String LOG_DIRECTORY = "/tmp";
-			
-			Rlogger.logger(LOG_LEVEL, LOG_DIRECTORY);
-//			Assert.assertEquals(Rlogger.getLog_level(), (LOG_LEVEL > 5) ? 5: LOG_LEVEL);
-			Assert.assertEquals(Rlogger.getLog_directory(), LOG_DIRECTORY);
-			Assert.assertNotNull("REXPLogger 1", Rlogger.logOptions.toDebugString());
-			System.out.println(Rlogger.logOptions.toDebugString());
+    try {
+      // Sample usage from Java variables setup
+      String LOG_LEVEL = "BLAH";
+      String LOG_DIRECTORY = "/tmp";
 
-			LOG_LEVEL = "DEBUG";
-			LOG_DIRECTORY = "/tmp";
-			
-			Rlogger.logger(LOG_LEVEL, LOG_DIRECTORY);
-//			Assert.assertEquals(Rlogger.getLog_level(), (LOG_LEVEL > 5) ? 5: LOG_LEVEL);
-			Assert.assertEquals(Rlogger.getLog_directory(), LOG_DIRECTORY);
-			Assert.assertNotNull("REXPLogger 2", Rlogger.logOptions.toDebugString());
-			System.out.println(Rlogger.logOptions.toDebugString());
+      Rlogger.logger(LOG_LEVEL, LOG_DIRECTORY);
+      Assert.assertEquals(Rlogger.getLogDirectory(), LOG_DIRECTORY);
+      Assert.assertNotNull("REXPLogger 1", Rlogger.getLogOptions().toDebugString());
+      System.out.println(Rlogger.getLogOptions().toDebugString());
 
-			// Default logging
-			Rlogger.logger();
-			System.out.println(Rlogger.logOptions.toDebugString());
-			
-			System.out.println("--- Done R Logging Test ---");
-		} catch (Exception e) {
-			e.printStackTrace();
-			Assert.fail("R Logging Failure");
-		}
+      LOG_LEVEL = "DEBUG";
+      LOG_DIRECTORY = "/tmp";
 
-	}
+      Rlogger.logger(LOG_LEVEL, LOG_DIRECTORY);
+      Assert.assertEquals(Rlogger.getLogDirectory(), LOG_DIRECTORY);
+      Assert.assertNotNull("REXPLogger 2", Rlogger.getLogOptions().toDebugString());
+      System.out.println(Rlogger.getLogOptions().toDebugString());
+
+      // Default logging
+      Rlogger.logger();
+      System.out.println(Rlogger.getLogOptions().toDebugString());
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      Assert.fail("Test Failed: " + ExceptionUtils.getFullStackTrace(e));
+    }
+  }
 
 }
