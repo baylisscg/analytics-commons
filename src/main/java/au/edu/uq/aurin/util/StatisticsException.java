62e1a3f39a209d1803d2769102c515ae0c89b990
pingupingu <irfan@itee.uq.edu.au>
2014-02-20 12:52:34 +1000

    remove logging from exception
    Change-Id: I25221ecf89b87018c2ab7866441ff9b958baf993

--- src/main/java/au/edu/uq/aurin/util/StatisticsException.java
+++ src/main/java/au/edu/uq/aurin/util/StatisticsException.java
@@ -1,34 +1,22 @@
 package au.edu.uq.aurin.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.apache.commons.lang.exception.ExceptionUtils;
-
 public class StatisticsException extends Exception {
 
   private static final long serialVersionUID = 1L;
-  private static final String MSGHEADER = "Statistics Exception: ";
 
-  private static final Logger LOG = LoggerFactory.getLogger(StatisticsException.class);
-
-  
   public StatisticsException() {
     super();
-    LOG.info(MSGHEADER);
   }
 
   public StatisticsException(String msg) {
     super(msg);
-    LOG.info(MSGHEADER + msg);
   }
 
   public StatisticsException(Throwable cause) {
     super(cause);
-    LOG.info(MSGHEADER + ExceptionUtils.getFullStackTrace(cause));
   }
 
   public StatisticsException(String msg, Throwable cause) {
     super(msg, cause);
-    LOG.info(MSGHEADER + msg + " " + ExceptionUtils.getFullStackTrace(cause));
   }
 }
