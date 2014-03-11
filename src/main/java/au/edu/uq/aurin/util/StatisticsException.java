33da9ad7751ba1313f4c03fd1fb20d5caed277f8
pingupingu <irfan@itee.uq.edu.au>
2014-03-11 10:56:32 +1000

    Documentation
    Change-Id: Id6c4368f8a318817ec45c7ef68e1cd10061adca7

--- src/main/java/au/edu/uq/aurin/util/StatisticsException.java
+++ src/main/java/au/edu/uq/aurin/util/StatisticsException.java
@@ -1,21 +1,44 @@
 package au.edu.uq.aurin.util;
 
+import au.edu.uq.aurin.interfaces.Statistics;
+
+/**
+ * {@link StatisticsException} class for the {@link Statistics} Interface
+ * 
+ * @author irfan
+ */
 public class StatisticsException extends Exception {
 
   private static final long serialVersionUID = 1L;
 
+  /**
+   * Default Exception without a custom message
+   */
   public StatisticsException() {
     super();
   }
 
+  /**
+   * Exception with a custom error message
+   * @param msg
+   */
   public StatisticsException(String msg) {
     super(msg);
   }
 
+  /**
+   * Exception with a cause
+   * @param cause
+   */
   public StatisticsException(Throwable cause) {
     super(cause);
   }
 
+  /**
+   * Exception with a message and a cause
+   * @param msg
+   * @param cause
+   */
   public StatisticsException(String msg, Throwable cause) {
     super(msg, cause);
   }
