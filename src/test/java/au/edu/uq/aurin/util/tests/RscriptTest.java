4d1b2c6b741b3b5556b366e7cdcd48720d3dd709
pingupingu <irfan@itee.uq.edu.au>
2014-03-12 12:57:40 +1000

    use this to test the updated R script
    Change-Id: I2a0ca664da35315c51edf783a5d19ebb7b9314a7

--- src/test/java/au/edu/uq/aurin/util/tests/RscriptTest.java
+++ src/test/java/au/edu/uq/aurin/util/tests/RscriptTest.java
@@ -1,16 +1,56 @@
 package au.edu.uq.aurin.util.tests;
 
+import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
 
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.junit.Assert;
 import org.junit.Test;
+import org.rosuda.REngine.REXP;
+import org.rosuda.REngine.REXPDouble;
+import org.rosuda.REngine.REXPInteger;
+import org.rosuda.REngine.REXPLogical;
+import org.rosuda.REngine.REXPMismatchException;
+import org.rosuda.REngine.RList;
+import org.rosuda.REngine.Rserve.RConnection;
+import org.rosuda.REngine.Rserve.RserveException;
 
+import au.edu.uq.aurin.logging.Rlogger;
 import au.edu.uq.aurin.util.Rscript;
 import au.edu.uq.aurin.util.StatisticsException;
 
 public class RscriptTest {
+
+  public double[] multiplier(double mult) {
+
+    double[] content = new double[] { 11, 22, 33, 44, 55, 66, 77, 88, 99, 00 };
+
+    for (int i = 0; i < content.length; i++) {
+      content[i] = mult * content[i];
+    }
+    return content;
+  }
+
+  public REXP dataGenerator() {
+
+    REXP data = null;
+
+    RList a = new RList();
+    // new test data dependents
+    for (int i = 0; i < 20; i++) {
+      a.put(("col" + i).toString(), new REXPDouble(multiplier(i + Math.random()
+          * 10)));
+    }
+
+    try {
+      data = REXP.createDataFrame(a);
+    } catch (REXPMismatchException e) {
+      e.printStackTrace();
+    }
+
+    return data;
+  }
 
   @Test
   public void test() {
@@ -18,6 +58,7 @@ public class RscriptTest {
     System.out.println("Rscript Unit Test");
 
     try {
+      // 0. Load the script
       String script = Rscript.load("/au/edu/uq/aurin/rscripts/script.r");
       Assert.assertNotNull("Unable to load given R script", script);
 
@@ -28,10 +69,34 @@ public class RscriptTest {
       // System.out.println(url.getFile());
       // }
 
+      // 1. create a connection
+      RConnection cIn = new RConnection();
+      cIn.assign("script", script);
+
+      // 2. setup the data inputs
+      cIn.assign("dataFrameName", "dataF");
+      cIn.assign("dataF", this.dataGenerator());
+
+      // 3. input options
+      RList optList = new RList();
+      optList.put("intercept", new REXPLogical(true));
+      cIn.assign("optionsM", REXP.createDataFrame(optList));
+
+      // 4. Logging setup
+      Rlogger.logger("DEBUG", "/tmp");
+      cIn.assign("optionsLogging", Rlogger.getLogOptions());
+
+      // 5. call the function defined in the script
+      REXP worker = cIn.eval("try(eval(parse(text=script)),silent=FALSE)");
+      System.out.println("worker result: " + worker.toDebugString());
+
+    } catch (RserveException x) {
+      Assert.fail("Test Failed: Rserve" + ExceptionUtils.getFullStackTrace(x));
+    } catch (REXPMismatchException e) {
+      Assert.fail("Test Failed: REXPMismatch" + ExceptionUtils.getFullStackTrace(e));
     } catch (StatisticsException e) {
       Assert.fail("Test Failed: " + ExceptionUtils.getFullStackTrace(e));
     }
-
   }
 
 }
