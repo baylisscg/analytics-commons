2a7ecd5c554fcbdf14603f32e8cf9dc688975023
pingupingu <irfan@itee.uq.edu.au>
2014-12-09 12:08:37 +1000

    [AUR-3632] R source formatting statistics
    Formatting R script file in analytics-commons test case
    
    Change-Id: I3d6ee34cac0473a1d7c27040cb2f5b27313f8cd9

--- src/test/resources/au/edu/uq/aurin/rscripts/script.r
+++ src/test/resources/au/edu/uq/aurin/rscripts/script.r
@@ -12,21 +12,21 @@ libraryError <- function() {
 
 # No logging scenario
 setupNullLogger <- function() {
-
+  
   lfile <- ""
   llvl <- 1
   rLog <- create.logger(logfile=lfile);
   level(rLog) <- verbosity(llvl) # log level 5 - FATAL or 6 - TRACE only
-
+  
   #  info(rLog, c("str(rlogger) = ", capture.output(str(rlogger))))
   print(paste("Logging LEVEL:", llvl, "Logging DIRECTORY:", lfile))
-
+  
   return(rLog)
 }
 
 # Logging to given directory and a log level
 setupFileLogger <- function(optionsLogging) {
-
+  
   lfile <- as.character(paste(optionsLogging$LOG_DIRECTORY,
                               .Platform$file.sep,
                               "rlogger.rlog", sep="", collpse=""))
@@ -37,19 +37,19 @@ setupFileLogger <- function(optionsLogging) {
   print("tupperLvl = "); print(tupperLvl)
   print("numLogLevel = "); print(numLogLevel)
   print("llvl = "); print(llvl)
-
+  
   rLog <- log4r::create.logger(logfile=lfile, level=llvl)
   #  print(paste("str(rLog) = ", capture.output(str(rLog))))
   #  info(rLog, paste("info:: str(rLog) = ", capture.output(str(rLog))))
   #  debug(rLog, paste("debug:: str(rLog) = ", capture.output(str(rLog))))
-
+  
   return(rLog)
 }
 
 # Main logger setup function
 # Supports optionsLogging Dataframe with: String LOG_LEVEL and LOG_DIRECTORY
 setupLogging <- function() {
-
+  
   libraryError()
   
   # check validity of optionsLogging
@@ -87,7 +87,7 @@ libraryError1 <- function(rLog) {
 
 ## library version check
 libraryVersionError <- function(rLog) {
-
+  
   if(compareVersion(toString(packageVersion("RJSONIO")), "1.0") == -1) {
     error(rLog, paste('Error library version: ', call.=print(traceback())))
     stop("Error Library version: ", call.=error(rLog, traceback()))
@@ -97,13 +97,13 @@ libraryVersionError <- function(rLog) {
 ## Check if the input is valid object type
 ## Make sure to catch the input errors early
 inputError <- function(rLog, dataFrameName, optionsM, optionsLogging) {
-
+  
   if(!(exists("dataFrameName") && exists("optionsM"))) {
     warning(paste("ERROR: Invalid inputs for: dataFrameName = ", dataFrameName, optionsM))
     error(rLog, paste("Invalid inputs: ", dataFrameName, optionsM))
     stop("ERROR: Invalid inputs: ", call.=print(paste(dataFrameName, optionsM)))
   }
-
+  
   if(!(is.character(dataFrameName) && length(dataFrameName) >= 1)) {
     error(rLog, paste("Invalid input, length: dataFrameName = ", dataFrameName))
     stop(paste("ERROR: Invalid input, length: dataFrameName = ", dataFrameName), call.=error(rLog, traceback()))
@@ -115,52 +115,52 @@ inputError <- function(rLog, dataFrameName, optionsM, optionsLogging) {
     error(rLog, paste("Invalid input, length: dataF = ", dataF))
     stop(paste("ERROR: Invalid input, length: dataF = ", dataF), call.=error(rLog, traceback()))
   }
-
+  
   if(!(is.data.frame(optionsM) && length(optionsM) >= 1)) {
     error(rLog, paste("ERROR: Invalid input, length: optionsM = ", optionsM))
   }
-
+  
   if(exists("optionsLogging")) {
     if(!(is.data.frame(optionsLogging) && length(optionsLogging) >= 1)) {
       error("Invalid input, length: optionsLogging = ", optionsLogging)
       stop("ERROR: Logging option INVALID", call.=print(paste("Invalid input, length: optionsLogging = ", optionsLogging)))
     }
   }
-
+  
   debug(rLog, paste("str(dataF) = ", capture.output(str(dataF))))
   debug(rLog, paste("dataF = ", capture.output(dataF)))
   info(rLog, paste("optionsM = ", optionsM))
-
+  
 }
 
 ########## METHOD_NAME STATISTIC COMPUTE ##########
 ## return 'NULL' in case of error
 METHOD_NAME <- function() {
-
+  
   oData <- NULL
-
+  
   # setup
   #optionsLogging <- data.frame(LOG_LEVEL="DEBUG", LOG_DIRECTORY="/tmp")
   rLog <- setupLogging()
   #  print('METHOD_NAME:: rLog = '); print(rLog)
   print("Now we can use rLog for logging")
-
+  
   libraryError1(rLog)
   inputError(rLog, dataFrameName, optionsM, optionsLogging)
   dataF <- get(dataFrameName)
   # Remove na
   dataF <- na.omit(dataF)
-
-
-	debug(rLog, "DEBUG:: Hello using the logger rLog")
-	info(rLog, "INFO:: Hello using the logger rLog")
-
+  
+  
+  debug(rLog, "DEBUG:: Hello using the logger rLog")
+  info(rLog, "INFO:: Hello using the logger rLog")
+  
   return(oData)
 }
 
 ## Test method with sample/dummy data to check working of algorithm
 TestMETHOD_NAME <- function() {
-
+  
   # setup inputs
   dataF <<- data.frame(
     col1=c(1.1, 2.2, 3.3, 11.1, 22.2, 33.3),
@@ -170,14 +170,14 @@ TestMETHOD_NAME <- function() {
   )
   dataFrameName <<- "dataF"
   dataF <<- get(dataFrameName)
-
+  
   optionsM <<- data.frame(
     intercept=TRUE
   )
-
+  
   optionsLogging <<- data.frame(LOG_LEVEL="DEBUG", LOG_DIRECTORY="/tmp")
-
-	METHOD_NAME()
+  
+  METHOD_NAME()
 }
 
 ## TEST RUN
